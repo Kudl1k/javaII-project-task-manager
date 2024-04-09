beaa9815e4ffeb74ce59d306818308a697f5e795
Kudl1k <stepanek.kudlacek@gmail.com>
2024-04-09 22:40:33 +0200

    updated task

--- src/main/java/cz/kudladev/taskmanager/model/Task.java
+++ src/main/java/cz/kudladev/taskmanager/model/Task.java
@@ -1,4 +1,24 @@
 package cz.kudladev.taskmanager.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
 public class Task {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String date;
+
+    private char progress;
+
+
 }
